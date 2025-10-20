6007a032ab3e9982a35a38c913e788e74c7922c2
artserpa <artserpac@gmail.com>
2025-10-20 10:38:25 -0300

    Update pytest for plot_w_distribution to test automatic n_bins calculation

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -7,6 +7,6 @@ def test_plot_w_distribution_runs():
     freq_X = np.array([0.1, 0.2, 0.5, 0.8])
     chain_lengths = np.array([5, 10, 2, 1])
 
-    plot_w_distribution(freq_X, chain_lengths, n_bins=10, smooth_window=5, poly_order=2, label="Test", color="blue")
+    plot_w_distribution(freq_X, chain_lengths, smooth_window=5, poly_order=2, label="Test", color="blue")
 
     plt.close()
